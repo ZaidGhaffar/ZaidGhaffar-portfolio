4c41f0d7ba210089db53d652fbeb590673ff2016
ZaidGhaffar <zaidghaffar.business@gmail.com>
2025-06-02 20:59:32 +0500

    fix: update fonts to use Inter and Roboto Mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
